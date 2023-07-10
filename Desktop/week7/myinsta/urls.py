26de3d520ea6ff83becfbe447d0f39f3ec274e76
newoceanwave <leeseohyun02@naver.com>
2023-07-10 19:51:23 +0900

    basic mission

---
+++ Desktop/week7/myinsta/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from posts.views import lotto
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('lotto/', lotto, name='lotto'),
+]
